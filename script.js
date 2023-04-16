ca8032bcb0d287c3d0740f86c6d8a741bf7c7875
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-04-16 22:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const ulElmt=document.getElementsByTagName("ul")[0];
+let displayText;
+
+
+for(let i=0;i<ulElmt.children.length;i++){
+	let node=ulElmt.children[i];
+	if(node.id=='level'){
+
+		
+	displayText="The level of the element is: "+(i+1)
+}
+}
+alert(displayText);
